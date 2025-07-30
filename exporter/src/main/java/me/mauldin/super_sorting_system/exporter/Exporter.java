b9eebb98c90b9cc5a84432809fcb921d51de2dbc
Willow Mauldin <willow@mauldin.me>
2025-07-29 20:51:31 -0700

    Add exporter mod for item data

---
+++ exporter/src/main/java/me/mauldin/super_sorting_system/exporter/Exporter.java
@@ -0,0 +1,56 @@
+package me.mauldin.super_sorting_system.exporter;
+
+import com.google.gson.Gson;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.Item;
+import net.minecraft.registry.BuiltinRegistries;
+import net.minecraft.registry.Registries;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class Exporter implements ModInitializer {
+	public static final String MOD_ID = "exporter";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Beginning export process");
+
+		ArrayList<ExportedItem> exportedItems = new ArrayList<>();
+
+		LOGGER.info("Exporting items");
+		for (var entry : Registries.ITEM.getEntrySet()) {
+			var item = entry.getValue();
+			var registryKey = entry.getKey();
+
+			int rawId = Item.getRawId(item);
+			String displayName = item.getName().getString();
+			String key = registryKey.getValue().toString();
+
+			exportedItems.add(new ExportedItem(rawId, displayName, key));
+		}
+
+		Gson gson = new Gson();
+		String export = gson.toJson(exportedItems);
+
+		Path writeToPath = FabricLoader.getInstance().getGameDir().resolve("item_export.json");
+
+		try {
+			FileUtils.writeStringToFile(new File(writeToPath.toUri()), export, "UTF-8");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		LOGGER.info("Item export successful. Written to {}", writeToPath.toAbsolutePath());
+	}
+}
+
+record ExportedItem(int rawId, String displayName, String key) {}
